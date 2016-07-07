52209f08f20a1378b5564eff65ad7bc56de7784b
greatermeans <gakouri@gmail.com>
2016-07-07 13:20:44 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 250}
+	validates :summary, length: {maximum: 250}
+	validates :clickbait, inclusion: { in: [true]}
+	validates :category, inclusion: {in: %w{Fiction Non-Fiction}}
+
+	def clickbait
+		["Won't Believe", "Secret", "Top", "Guess"].any? { |w| title =~ /#{w}/ }
+	end
+
 end
